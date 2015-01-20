03cb97d19c2b3358a24193896a0f1c1e5943d688
jagdeep singh <singhjess@gmail.com>
2015-01-20 11:29:07 -0500

    comitting

--- countries/views.py
+++ countries/views.py
@@ -1,12 +1,19 @@
+from _io import BytesIO
+import json
+
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User, Group
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, views, status
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 
 from countries.models import Job
 from countries.serializers import UserSerializer, GroupSerializer, JobSerializer
 
 from .forms import JobForm
+from rest_framework.permissions import AllowAny
 
 
 class JobList(generics.ListCreateAPIView):
@@ -17,7 +24,48 @@ class JobList(generics.ListCreateAPIView):
 class JobDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+    
+    
+    
+    
+    
+class LoginView(views.APIView):
+    
+    permission_classes = (AllowAny,)
+    
+    def post(self, request, format=None):
+            
+        s = str(request.DATA)
+        s = s.replace("u'", "\"");
+        s= s.replace("'", "\""); 
+        
+        print s
+        
+        data = json.loads(s)
+        
+        email = data.get('email', None)
+        password = data.get('password', None)
+        
+        account = authenticate(username=email, password=password)
+        
 
+        if account is not None:
+            #Console.write(self, " logged in " + account)
+            if account.is_active:
+                login(request, account)
+
+              
+                return Response("hi")
+            else:
+                return Response({
+                    'status': 'Unauthorized',
+                    'message': 'This account has been disabled.'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({
+                'status': 'Unauthorized',
+                'message': 'Username/password combination invalid.'
+            }, status=status.HTTP_401_UNAUTHORIZED)
 
 
 # Create your views here.
--- myjango/urls.py
+++ myjango/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from rest_framework import routers
 
 from countries import views
-from countries.views import JobFormView
+from countries.views import JobFormView, LoginView
 
 
 router = routers.DefaultRouter()
@@ -15,7 +15,8 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-      url(r'^job/$', views.JobList.as_view()),
+    url(r'^job/$', views.JobList.as_view()),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),  
     url(r'^job/(?P<pk>[0-9]+)/$', views.JobDetail.as_view()),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^job-form/$',login_required(JobFormView.as_view()), name='job_form')
